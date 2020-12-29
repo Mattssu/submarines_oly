93ee87dd21f2c211f53a3072ebdf3915d2248467
Matssu <shednic55@gmail.com>
2020-12-29 11:35:42 +0200

    added base classes

---
+++ SubmarineGameClient.py
@@ -0,0 +1,17 @@
+class GameClient:
+    def __init__(self):
+        # TCPCommunicationHandler
+        # SubmarineGameManager
+        pass
+
+    def submarine_location_init(self):
+        pass
+
+    def choose_host_or_client(self):
+        pass
+
+    def connect_to_host(self):
+        pass
+
+    def listen_to_connections(self):
+        pass
---
+++ SubmarineGameManager.py
@@ -0,0 +1,16 @@
+GAME_SIZE = 10
+
+
+class SubmarineGame:
+    def __init__(self):
+        game_map_size = GAME_SIZE
+        pass
+
+    def init_map(self):
+        pass
+
+    def init_submarine_locations(self):
+        pass
+
+    def bomb_location(self, location):
+        pass
